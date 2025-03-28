36128f8bd120c9ea8b9cbb0cb32fb353c4970dfb
top1dash <austin.chase@archwaycapitalgroup.com>
2025-03-28 16:33:34 -0600

    Update _app.js add navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 // pages/_app.js
 import '../styles/globals.css';
+import Navbar from '../components/ui/Navbar';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
