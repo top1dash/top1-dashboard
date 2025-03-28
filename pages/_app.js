efa337fc454319da79f1b17e1e48b5c85d46bc69
top1dash <austin.chase@archwaycapitalgroup.com>
2025-03-28 16:38:22 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import '../styles/globals.css';
-import Navbar from '../components/ui/Navbar';
+import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
