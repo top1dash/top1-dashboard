dbbaa2474d4516ac793d9993c24da175cc5e806a
top1dash <austin.chase@archwaycapitalgroup.com>
2025-03-29 09:57:03 -0600

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -70,3 +70,23 @@ export default function Login() {
           <button
             type="submit"
             className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-md transition"
+          >
+            Log In
+          </button>
+        </form>
+
+        <div className="mt-4 text-center">
+          <p className="text-sm text-gray-600">
+            New user?{' '}
+            <a
+              href="/signup"
+              className="text-blue-600 hover:text-blue-800 font-medium underline"
+            >
+              Click here to sign up
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
