5a43c75ef008add6913c37e749d8bcb6fcc67b04
top1dash <austin.chase@archwaycapitalgroup.com>
2025-04-04 22:08:52 -0600

    Create countryToRegionMap.js

---
+++ components/ui/countryToRegionMap.js
@@ -0,0 +1,95 @@
+// utils/countryToRegionMap.js
+
+const countryToRegionMap = {
+  // North America
+  US: "us",
+  CA: "mexico_canada",
+  MX: "mexico_canada",
+
+  // South America
+  AR: "south_america",
+  BR: "south_america",
+  CO: "south_america",
+  CL: "south_america",
+  PE: "south_america",
+  VE: "south_america",
+  UY: "south_america",
+  PY: "south_america",
+  BO: "south_america",
+  EC: "south_america",
+  SR: "south_america",
+
+  // Europe
+  GB: "europe",
+  FR: "europe",
+  DE: "europe",
+  IT: "europe",
+  ES: "europe",
+  NL: "europe",
+  SE: "europe",
+  NO: "europe",
+  FI: "europe",
+  PL: "europe",
+  RO: "europe",
+  RU: "europe",
+  UA: "europe",
+  CH: "europe",
+  BE: "europe",
+  DK: "europe",
+  PT: "europe",
+  CZ: "europe",
+  GR: "europe",
+  HU: "europe",
+  SK: "europe",
+  BG: "europe",
+  IE: "europe",
+  SI: "europe",
+  HR: "europe",
+
+  // Asia
+  CN: "asia",
+  IN: "asia",
+  JP: "asia",
+  KR: "asia",
+  ID: "asia",
+  PH: "asia",
+  TH: "asia",
+  VN: "asia",
+  MY: "asia",
+  PK: "asia",
+  BD: "asia",
+  SA: "asia",
+  AE: "asia",
+  SG: "asia",
+  KZ: "asia",
+
+  // Africa
+  ZA: "africa",
+  NG: "africa",
+  KE: "africa",
+  EG: "africa",
+  MA: "africa",
+  DZ: "africa",
+  ET: "africa",
+  GH: "africa",
+  TN: "africa",
+  SN: "africa",
+
+  // Oceania
+  AU: "oceania",
+  NZ: "oceania",
+  PG: "oceania",
+  FJ: "oceania",
+
+  // Caribbean
+  JM: "caribbean",
+  HT: "caribbean",
+  TT: "caribbean",
+  BB: "caribbean",
+  DO: "caribbean",
+  BS: "caribbean",
+  LC: "caribbean",
+  GD: "caribbean",
+};
+
+export default countryToRegionMap;
