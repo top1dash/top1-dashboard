19aad592c7ba6c04e15a59756638b51bc8baf64b
top1dash <austin.chase@archwaycapitalgroup.com>
2025-04-04 22:28:00 -0600

    Update ZipCityAutocompleteInput.js

--- components/ui/ZipCityAutocompleteInput.js
+++ components/ui/ZipCityAutocompleteInput.js
@@ -1,8 +1,8 @@
 import { useEffect, useState, useRef } from "react";
 import Fuse from "fuse.js";
 import { inflate } from "pako";
-import countryToRegionMap from "../../countryToRegionMap";
-import isoCountryMap from "../../isoCountryMap"; // adjust path as needed
+import countryToRegionMap from "./countryToRegionMap";
+import isoCountryMap from "./isoCountryMap"; // adjust path as needed
 
 
 export default function ZipCityAutocompleteInput({ questionId, onChange }) {
