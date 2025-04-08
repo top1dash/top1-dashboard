ce4fcb972c768ab14c3de4daac6d40dc5a215504
top1dash <austin.chase@archwaycapitalgroup.com>
2025-04-07 21:28:24 -0600

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -209,6 +209,7 @@ export default function Dashboard() {
               </div>
 
             {/* Physical Appearance Section */}
+           <div className="col-span-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mt-8">
               {/* Vertical Filter Panel */}
               <div className="col-span-1">
