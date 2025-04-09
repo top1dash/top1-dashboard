78545732caba70ec294d50cb973d333dce54edb3
top1dash <austin.chase@archwaycapitalgroup.com>
2025-04-09 10:18:59 -0600

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-white dark:bg-gray-950 text-gray-900 dark:text-white">
       <Navbar />
-      <main className="max-w-5xl mx-auto px-4 py-8">
+      <main className="w-full px-4 py-8">
         {children}
       </main>
     </div>
