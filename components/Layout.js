df5bff5e70ef74b0cb7616a07b89f18699e8cda3
top1dash <austin.chase@archwaycapitalgroup.com>
2025-04-09 13:02:26 -0600

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 // components/Layout.js
 import Navbar from './Navbar'
 
-export default function Layout({ children }) {
+export default function Layout({ children, noBackground }) {
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-950 bg-[url('/top1percent.png')] dark:bg-none bg-top bg-no-repeat bg-cover text-gray-900 dark:text-white">
+    <div className={`min-h-screen ${
+      noBackground
+        ? 'bg-gray-100 dark:bg-gray-950'
+        : 'bg-gray-100 dark:bg-gray-950 bg-[url("/top1percent.png")] dark:bg-none bg-top bg-no-repeat bg-cover'
+    } text-gray-900 dark:text-white`}>
       <Navbar />
       <main className="w-full px-4 py-8">
         {children}
