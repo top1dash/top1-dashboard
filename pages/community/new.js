e5b6b52390eff3a9cd7b9965fd8735a75fe6441c
top1dash <austin.chase@archwaycapitalgroup.com>
2025-04-11 09:38:39 -0600

    Update new.js

--- pages/community/new.js
+++ pages/community/new.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { supabase } from "../../supabaseClient";
+import Layout from "../../components/Layout";
 
 export default function NewPost() {
   const [title, setTitle] = useState("");
